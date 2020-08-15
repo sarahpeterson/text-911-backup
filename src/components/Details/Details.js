eb24fb295a183f2a7964b49159fa0263792d5440
Sarah Peterson <sarahp@nurelm.com>
2020-08-15 10:20:46 -0400

    changes detail component to class to be able to use state

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Debounce } from 'react-throttle';
@@ -10,162 +10,211 @@ import DetailModal from './DetailModal';
 import './Details.css';
 import Modal from '../Common/Modal';
 
-const onSuccess = (emergency, location, detail, props) => {
-  const { long, lat, address } = props;
-  const body = `${emergency}, ${location}: ${detail} at ${long}, ${lat}, ${address}`;
-  console.log(body)
-  return fetch('/api/messages/',
-  {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(
-      {
-        to: +4156345171,
-        body: body
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emergency: false,
+      location: false,
+      icon: '',
+      emergencyDetail: '',
+      locationDetail: '',
+      more: '',
+      status: '',
+      modal: false
+    };
+  }
+
+  onSuccess(emergency, location, detail) {
+    const { long, lat, address } = this.props;
+    const body = `${emergency}, ${location}: ${detail} at ${long}, ${lat}, ${address}`;
+    console.log(body)
+    return fetch('/.netlify/functions/text-created',
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    ),
-  })
-  .then((response) => {
-    console.log(response)
-    if (response.status === 200) {
-      console.log('success');
+      body: JSON.stringify(
+        {
+          body: body
+        },
+      ),
+    })
+    .then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        this.setState({ modal: true, status: 'success' });
+      } else {
+        this.setState({ modal: true, status: 'error' });
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.setState({ modal: true, status: 'error' });
+    })
+  }
+
+  maybeModal() {
+    const { modal, status } = this.state;
+    console.log(modal, status)
+    if (modal && status === 'success') {
       return (
         <Modal
-          header="Message Sent!"
+          header="Your message was sent"
           para=""
-          btnTxt="Done"
-          btnClick={() => console.log('ok')}
+          btnTxt="Ok"
+          btnClick={() => {
+            this.setState({ modal: false, status: '' });
+          }}
         />
       )
-    } else {
-      console.log('else')
-    }
-  })
-}
-
-const Details = (props) => {
-  const [emergency, setEmergency] = useState(false);
-  const [location, setLocation] = useState(false);
-  const [icon, setIcon] = useState('');
-  const [emergencyDetail, setEDetail] = useState('');
-  const [locationDetail, setLDetail] = useState('');
-  const [more, moreInfo] = useState('');
-  return (
-    <div>
-      <Top />
-      <div className="detail-container">
-        <h3 className="detail-message">
-          Message Recieved
-        </h3>
-        <p className="detail-text">
-          Please check your text messages for a response from your nearest 911 center. Turn your phone on silent mode.
-        </p>
-        <p className="detail-text-bold">
-          If you do not receive a response, please call 911.
-        </p>
-      </div>
-      <div className="more-info-container">
-        <div className="more-info-wrapper">
-          <h2 className="more-info-text">
-            Send More Information
-          </h2>
-        </div>
-        <div className="optional-container">
-          <p className="optional-text">
-            Optional
-          </p>
-        </div>
-        <button
-          onClick={() => setEmergency(true)}
-          className="emergency-container"
-        >
-          <img
-            src={Police}
-            alt="police car"
-            className="icon"
-            title="Police Car Icon"
-          />
-          <p className="emergency-text">
-            Specific Emergency?
-          </p>
-        </button>
-        <button
-          onClick={() => setLocation(true)}
-          className="location-container"
-        >
-          <img
-            src={RedCross}
-            alt="red cross"
-            className="icon"
-            title="Red Cross Icon"
-          />
-          <p className="emergency-text">
-            Specific Location?
-          </p>
-        </button>
-      </div>
-      <div className="send-btn-container">
-        <button
-          onClick={() => onSuccess(emergencyDetail, locationDetail, more, props)}
-          tabIndex="1"
-          className="result-item"
-          style={{
-            backgroundColor: 'blue',
-            width: 300,
-            borderRadius: 25/2,
-            marginTop: 40
+    } if (modal && status === 'error') {
+      return (
+        <Modal
+          header="There was an error sending your message"
+          para=""
+          btnTxt="Ok"
+          btnClick={() => {
+            this.setState({ modal: false, status: '' });
           }}
-        >
-          <p className="btn-container">
-            Send Message
+        />
+      )
+    }
+  }
+
+  render() {
+    const {
+      emergencyDetail,
+      locationDetail,
+      more,
+      icon,
+      location,
+      emergency,
+      // modal
+    } = this.state;
+    const { modal, status } = this.state;
+    console.log(modal && status !== '')
+    return (
+      <div>
+        {
+          modal && status !== '' ?
+          <div className="modal-container" /> :
+          <div />
+        }
+        <Top />
+        <div className="detail-container">
+          <h3 className="detail-message">
+            Message Recieved
+          </h3>
+          <p className="detail-text">
+            Please check your text messages for a response from your nearest 911 center. Turn your phone on silent mode.
           </p>
-        </button>
+          <p className="detail-text-bold">
+            If you do not receive a response, please call 911.
+          </p>
+        </div>
+        <div className="more-info-container">
+          <div className="more-info-wrapper">
+            <h2 className="more-info-text">
+              Send More Information
+            </h2>
+          </div>
+          <div className="optional-container">
+            <p className="optional-text">
+              Optional
+            </p>
+          </div>
+          {this.maybeModal()}
+          <button
+            onClick={() => this.setState({ emergency: true })}
+            className="emergency-container"
+          >
+            <img
+              src={Police}
+              alt="police car"
+              className="icon"
+              title="Police Car Icon"
+            />
+            <p className="emergency-text">
+              Specific Emergency?
+            </p>
+          </button>
+          <button
+            onClick={() => this.setState({ location: true })}
+            className="location-container"
+          >
+            <img
+              src={RedCross}
+              alt="red cross"
+              className="icon"
+              title="Red Cross Icon"
+            />
+            <p className="emergency-text">
+              Specific Location?
+            </p>
+          </button>
+        </div>
+        <div className="send-btn-container">
+          <button
+            onClick={() => this.onSuccess(emergencyDetail, locationDetail, more)}
+            tabIndex="1"
+            className="result-item"
+            style={{
+              backgroundColor: 'blue',
+              width: 300,
+              borderRadius: 25/2,
+              marginTop: 40
+            }}
+          >
+            <p className="btn-container">
+              Send Message
+            </p>
+          </button>
+        </div>
+        <DetailModal
+          show={emergency}
+          buttons={[
+            'Unconscious',
+            'Domestic Violence',
+            'Crisis Intervention',
+            'Bleeding',
+            'Car Accident',
+            'Home Invasion/Theft',
+            'Shooting',
+            'Trouble Breathing'
+          ]}
+          title="Specific Emergency?"
+          setIcon={(b) => {
+            this.setState({ icon: icon === b ? '' : b });
+            this.setState({ emergencyDetail: b });
+            console.log('BBBB', b)
+          }}
+          iconChecked={icon}
+          dismiss={() => this.setState({ emergency: false })}
+        />
+        <DetailModal
+          show={location}
+          setIcon={(b) => {
+            this.setState({ icon: icon === b ? '' : b });
+            this.setState({ locationDetail: b });
+            console.log('BBBB', b)
+          }}
+          iconChecked={icon}
+          location
+          buttons={[
+            'Room Number',
+            'Floor',
+            'Bus/Train',
+            'Highway'
+          ]}
+          title="Specific Location?"
+          setDetail={(d) => this.setState({ more: d })}
+          dismiss={() => this.setState({ location: false })}
+        />
       </div>
-      <DetailModal
-        show={emergency}
-        buttons={[
-          'Unconscious',
-          'Domestic Violence',
-          'Crisis Intervention',
-          'Bleeding',
-          'Car Accident',
-          'Home Invasion/Theft',
-          'Shooting',
-          'Trouble Breathing'
-        ]}
-        title="Specific Emergency?"
-        setIcon={(b) => {
-          setIcon(b);
-          setEDetail(b);
-          console.log('BBBB', b)
-        }}
-        iconChecked={icon}
-        dismiss={() => setEmergency(false)}
-      />
-      <DetailModal
-        show={location}
-        setIcon={(b) => {
-          setIcon(b);
-          setLDetail(b);
-          console.log('BBBB', b)
-        }}
-        iconChecked={icon}
-        location
-        buttons={[
-          'Room Number',
-          'Floor',
-          'Bus/Train',
-          'Highway'
-        ]}
-        title="Specific Location?"
-        setDetail={(d) => moreInfo(d)}
-        dismiss={() => setLocation(false)}
-      />
-    </div>
-  );
+    );
+  }
 };
 
 const mapStateToProps = (state) => {
